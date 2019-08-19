dde3913e25066207db3a28772c6f635f9aba099a
Mars Lee <jlee10@brockport.edu>
2019-08-19 18:12:18 -0400

    added bals

--- main.js
+++ main.js
@@ -1,77 +1,107 @@
-// frog onclick
 
-// const frogs = ['red-frog', ];
-// const frogsJSObj = [];
+    let frogs = document.querySelectorAll('.frog')
 
-let cMajorScale = [cNote, dNote, eNote, dNote, fNote, gNote, aNote, cNote, highCNote,]
-
-// C major scale notes
-let cNote = new Audio('cNote.mp3');
-// bNote.play();
-let dNote = // sound file
-
-let frogs = document.querySelectorAll('.frog')
-// frogs.dataset.key
-// frogs.dataset.note
-
-/* sample code I found online
-function play(){
-    var audio = document.getElementById("audio");
-    audio.play();
-}
-*/
-
-function playNote(e) 
-
-// var audio = document.getElementById("audio");
-// doucment.getAttribute("id")
-let holdStart, holdCheck, holdTime = null;
-eventAssignment = function(frog) {
-    frog.onmouseover = function(event) {
-        // change frog expression from closed to open eyes
+    function playNote(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        audio.play();
     }
-    frog.onmousedown = function(event) {
-        // check how much time has elapsed since holdStart to change frog expression
-        holdTime = performance.now()
-        if (holdTime = 0) { // when frog is clicked
-            // frog puffs up a little
-        } else if (holdTime > 1000 && holdTime < 2000) {
-            // frog puffs up more
-        } else {
-            // frog fully puffed
+    
+    // let gruntSound = new Audio('grunt.mp3')
+    // let highGruntSound = new Audio('highGrunt.mp3')
+    
+    let holdTime = null;
+    
+    eventAssignment = function(frog) {
+        frog.onkeydown = function(e) {
+            console.log(${e})
+            holdTime = performance.now() // performance.now() records miliseconds
+            if (holdTime = 0) {
+                // frog puffs up a little frog.src = ""
+            } else if (holdTime > 1000 && holdTime < 2000) {
+                // frog puffs up more
+            } else {
+                // frog fully puffed
+            }
+        }
+        frog.onkeyup = function(e) {
+            if (holdTime < 1000) {
+                // gruntSound.play();
+                frog.style.bottom = '200px'; // frog jumps little, from bottom:50px(defined in CSS) to 200px
+            } else if (holdTime > 1000 && holdTime < 2000) {
+                // higherGruntSound.play(); frog grunts at higher pitch
+                frog.style.bottom = '300px'; // frog jumps middle
+            } else {
+                // fplayNote(e) frog plays assigned note
+                frog.style.bottom = '400px'; // frog jumps high
+            }
         }
     }
-    frog.onmouseup = function(event) {
-        if (holdTime < 1000) {
-            // frog grunts
-            // frog jumps little
-        } else if (holdTime > 1000 && holdTime < 2000) {
-            // frog grunts, higher pitch
-            // frog jumps middle
-        } else {
-            // frog plays assigned note
-            // frog jumps high
-        }
+    
+frogs.forEach(eventAssignment)
+
+keydownFunction(function(event) {
+    event.style.bottom = '200px';
+})
+frogs.addEventListeners('keydown', keydownFunction)
+
+
+// frogs.onkeydown = () => {
+
+// }
+
+// frogs.onkeyup = () => {
+//     frog.style.bottom = '200px'
+// } 
+    
+    /*
+    Notes
+    To access data-* attribute from HTML
+    frogs.dataset.key, returns data-key
+    
+    
+    Explanation of playNote function
+    function playNote(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        audio.play();
     }
-}
+    AIM: press A key, play C note
+    A key has keycode of 65
+    thus keyCode = "65", play C note
+    e.keyCode returns JS keycode. so if press A key, e.keyCode returns 65
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${65}"]`);
+    const audio = document.querySelector(`audio[data-key="65"]`);
+    thus audio JS variable = entire HTML element with the attribute data-key=65
+        HTML element: <audio data-key="65" src="sounds/c.mp3"></audio>
+    
+    audio.play()
+    will thus play C, by playing the value of src, which is sounds/c.mp3
+    
+    Syntax explanation:
+    Backticks is for template literals/string interpolation
+        Example of template literals
+            const var = 'test'
+            console.log(`The value of var is ${myVariable}`) // prints The value of var is test
+    Not using "" double quotes because that would hard-code that specific classname
+        Example with "" double quotes
+        HTML: <p class="example">A paragraph</h2>
+        JS:   document.querySelector(".example")
+    
+    Attribute selectors syntax using [] square brackets
+    
+    Example of playing an audio file through DOM
+    function play(){
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
+    
+    Not using Date.now() because measures timestamp rather than seconds
 
-frog.forEach(eventAssignment)
 
-/*
-when red frog is clicked play cNote
-when red frog is clicked play cMajorScale[0]
-
-frog.addEventListener('mousedown', function(event) {
-    holdStart = Date.now()
-});
-
-frog.addEventListener('mouseup', function(event) {
-    holdTime = Date.now() - holdStart;
-  });
-
-let holdStart, holdTime = null;
-holdStart = Date.now()
-holdTime = Date.now() - holdStart
-
-now in holdTime you have mousedown time in milliseconds
-*/
+    // Instead of having to get each element by ID, you simply listen for a keypress
+// Example of getting by ID
+    // In HTML, first frog has id='0'
+    // In JS, when press first frog, play cMajorScale[0]
+    // let cMajorScale = [cNote, dNote, eNote, dNote, fNote, gNote, aNote, cNote, highCNote,]
+    // let cNote = new Audio('cNote.mp3')
+    */
